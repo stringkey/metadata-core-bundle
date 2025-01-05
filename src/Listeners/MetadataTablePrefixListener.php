a3eeecefe1aee1eac94ee8950c1e529df04babcf
peer <stringkey@wxs.nl>
2025-01-05 12:06:09 +0100

    Added a table prefix listener to allow metadata related bundles to be prefixed based on the class metadata

---
+++ src/Listeners/MetadataTablePrefixListener.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace Stringkey\MetadataCoreBundle\Listener;
+
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadata;
+
+class MetadataTablePrefixListener
+{
+    private string $prefix;
+
+    public function __construct(string $prefix)
+    {
+        $this->prefix = $prefix;
+    }
+
+    public function getSubscribedEvents(): array
+    {
+        return ['loadClassMetadata'];
+    }
+
+    // todo: find a smarter way to handle the prefix
+    // maybe by having the service and the listener in the shared bundle but the configuration in the separate bundles
+    // with the applicable class(es) configured
+
+    private const bundles = [
+        'Stringkey\MetadataCoreBundle\Entity'
+    ];
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs): void
+    {
+        $classMetadata = $eventArgs->getClassMetadata();
+
+        if (!in_array($classMetadata->namespace, self::bundles)) {
+            return;
+        }
+
+        if (!$classMetadata->isInheritanceTypeSingleTable() || $classMetadata->getName() === $classMetadata->rootEntityName) {
+            $classMetadata->setPrimaryTable([
+                'name' => $this->prefix . $classMetadata->getTableName()
+            ]);
+        }
+
+        $associationMappings = $classMetadata->getAssociationMappings();
+        foreach ($associationMappings as $fieldName => $mapping) {
+            if ($mapping['type'] == ClassMetadata::MANY_TO_MANY && $mapping['isOwningSide']) {
+                $mappedTableName = $mapping['joinTable']['name'];
+                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
+            }
+        }
+    }
+}
+
